e2f5e07118876be61f739d98cd044b7a4739581e
xiaonian0430 <xiaonian0430@163.com>
2021-11-05 14:10:41 +0800

    Update Log.php

---
+++ hiper-frame/Log/Log.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author: Xiao Nian
+ * @contact: xiaonian030@163.com
+ * @datetime: 2019-12-01 14:00
+ */
+namespace HP\Log;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+/**
+ * 日志
+ *
+ * @method static void log($level, $message, array $context = [])
+ * @method static void debug($message, array $context = [])
+ * @method static void info($message, array $context = [])
+ * @method static void notice($message, array $context = [])
+ * @method static void warning($message, array $context = [])
+ * @method static void error($message, array $context = [])
+ * @method static void critical($message, array $context = [])
+ * @method static void alert($message, array $context = [])
+ * @method static void emergency($message, array $context = [])
+ */
+class Log {
+    private static $instance;
+    public function __construct() {
+        if(!isset(static::$instance)){
+            static::$instance = new Logger('name');
+            static::$instance->pushHandler(new StreamHandler(CONFIG['LOG_PATH'], Logger::WARNING));
+        }
+        return static::$instance;
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return static::$instance->{$name}(... $arguments);
+    }
+}
