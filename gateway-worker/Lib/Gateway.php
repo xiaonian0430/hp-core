9d9600a0ce731da4d3169251fa11651e90a885ac
xiaonian0430 <xiaonian0430@163.com>
2021-12-14 09:16:01 +0800

    Add business worker connected and disconnected events
    Add business worker connected and disconnected events

--- gateway-worker/Lib/Gateway.php
+++ gateway-worker/Lib/Gateway.php
@@ -53,7 +53,7 @@ class Gateway
      * @var bool
      */
     public static $persistentConnection = false;
-
+    
     /**
      * 向所有客户端连接(或者 client_id_array 指定的客户端连接)广播消息
      *
@@ -169,7 +169,7 @@ class Gateway
     {
         return (int)static::getClientIdByUid($uid);
     }
-
+    
     /**
      * 判断client_id对应的连接是否在线
      *
@@ -666,7 +666,7 @@ class Gateway
 
     /**
      * 生成验证包，用于验证此客户端的合法性
-     *
+     * 
      * @return string
      */
     protected static function generateAuthBuffer()
@@ -1056,7 +1056,7 @@ class Gateway
         }
         static::setSocketSession($client_id, Context::sessionEncode($session));
     }
-
+    
     /**
      * 更新 session，实际上是与老的session合并
      *
@@ -1073,12 +1073,12 @@ class Gateway
         }
         static::sendCmdAndMessageToClient($client_id, GatewayProtocol::CMD_UPDATE_SESSION, '', Context::sessionEncode($session));
     }
-
+    
     /**
      * 获取某个client_id的session
      *
      * @param string   $client_id
-     * @return mixed false表示出错、null表示用户不存在、array表示具体的session信息
+     * @return mixed false表示出错、null表示用户不存在、array表示具体的session信息 
      */
     public static function getSession($client_id)
     {
